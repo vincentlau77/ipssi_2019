a9571f9d9e89603cf73ad7a851479b5af683971f
root <root@DESKTOP-IN3B8JU.localdomain>
2019-11-07 11:40:40 +0100

    ajout des scripts python

---
+++ calcul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def addition(a,b):
+    return(a+b)
+
+def soustraction(a,b):
+    return(a-b)
+
+def multiplication(a,b):
+    return(a*b)
+
+def division(a,b):
+    return(a/b)
---
+++ fonction.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import calcul
+
+print(calcul.addition(5,6))
+
+print(calcul.soustraction(7,2))
+
+print(calcul.multiplication(5,9))
+
+print(calcul.division(8,4))
