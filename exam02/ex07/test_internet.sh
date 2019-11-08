f6013e542540c2ec024c55dc5986d2287b6164f0
root <root@DESKTOP-IN3B8JU.localdomain>
2019-11-08 15:14:22 +0100

    exo7

--- exam02/ex07/test_internet.sh
+++ exam02/ex07/test_internet.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z www.google.com ] || curl -s -I www.google.com >/dev/null ;then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') internet OK" >> internet.log
+else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') internet FAIL" >> internet.log
+fi
 
-
-curl www.google.com
+exit
